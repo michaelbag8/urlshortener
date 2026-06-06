14937a8b330e91f9f0d8823f6b987b154b5aa041
michaelbag8 <michaelbag8@gmail.com>
2026-06-07 00:25:17 +0100

    models added

---
+++ models.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var urlStore = map[string]URL{}
+
+type URL struct {
+    OriginalURL  string
+    Clicks       int
+    CreatedAt    time.Time
+    ExpiresAt    time.Time
+    ShortCode    string
+}
+
+func generateShortCode() string {
+	code := make([]byte, 6)
+	for i := range code {
+		code[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(code)
+}
